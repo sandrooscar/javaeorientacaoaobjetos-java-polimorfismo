f911df1947c68f94a47070d8685d75d26819bfb9
Sandro <sandro.bugmann@posthaus.com.br>
2022-11-17 16:20:11 -0300

    Introdução a herança

---
+++ src/br/com/bytebank/Gerente.java
@@ -0,0 +1,18 @@
+package br.com.bytebank;
+
+public class Gerente extends Funcionario {
+	
+	private int senha;
+	
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		}
+		return false;
+	}
+
+}
